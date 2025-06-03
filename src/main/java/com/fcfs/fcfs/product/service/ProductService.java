5837b10b47010aa79c3da76c1cd457f80fb332f5
SUICHO <65757661+whtndls@users.noreply.github.com>
2025-06-04 06:28:31 +0900

    feat: 서비스 인터페이스 추출

--- src/main/java/com/fcfs/fcfs/product/service/ProductService.java
+++ src/main/java/com/fcfs/fcfs/product/service/ProductService.java
@@ -1,37 +1,11 @@
 package com.fcfs.fcfs.product.service;
 
 import com.fcfs.fcfs.product.dto.response.ProductResponseDto;
-import com.fcfs.fcfs.product.entity.Product;
-import com.fcfs.fcfs.product.repository.ProductRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-@Transactional(readOnly = true)
-public class ProductService {
+public interface ProductService {
+    List<ProductResponseDto> listProducts();
 
-    private final ProductRepository productRepository;
-
-    public List<ProductResponseDto> listProducts() {
-        List<Product> products = productRepository.findAll();
-
-        List<ProductResponseDto> list = new ArrayList<>();
-        for (Product product : products) {
-            list.add(ProductResponseDto.toDto(product));
-        }
-        return list;
-    }
-
-    public ProductResponseDto detailProduct(Long productId) {
-        return ProductResponseDto.toDto(
-                productRepository.findById(productId).orElseThrow(
-                        () -> new IllegalArgumentException("존재하지 않는 상품입니다.")
-                )
-        );
-    }
+    ProductResponseDto detailProduct(Long productId);
 }
