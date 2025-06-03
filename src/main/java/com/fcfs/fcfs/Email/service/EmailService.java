ba2e572899bf36d716d3dd04b673de457e9647af
SUICHO <65757661+whtndls@users.noreply.github.com>
2025-06-04 05:12:45 +0900

    fix: extract serviceImpl interface

--- src/main/java/com/fcfs/fcfs/Email/service/EmailService.java
+++ src/main/java/com/fcfs/fcfs/Email/service/EmailService.java
@@ -1,46 +1,13 @@
 package com.fcfs.fcfs.Email.service;
 
 import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
-import jakarta.mail.internet.MimeUtility;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.stereotype.Service;
 
 import java.io.UnsupportedEncodingException;
 
-@Service
-@RequiredArgsConstructor
-public class EmailService {
-
-    // EmailConfig가 주입됨
-    private final JavaMailSender mailSender;
-
-    @Value("${spring.mail.username}")
-    private String fromAddress;
+public interface EmailService {
 
     // 보내려는 이메일, 제목, 본문만 입력하면 무슨 이메일이든 보낼 수 있게 설정해두었다.
-    public void sendVerificationEmail(String toEmail,
-                                      String title,
-                                      String htmlContent) throws MessagingException, UnsupportedEncodingException {
-
-        MimeMessage message = mailSender.createMimeMessage();
-
-        MimeMessageHelper helper = new MimeMessageHelper(
-                message,
-                MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
-                "UTF-8"
-        );
-
-        String encodedTitle = MimeUtility.encodeText(title, "UTF-8", "B");
-        helper.setTo(toEmail);
-        helper.setSubject(encodedTitle);
-        helper.setFrom(fromAddress);
-        helper.setText(htmlContent, true); // 두 번째 파라미터를 `true`로 하면 HTML로 전송됩니다.
-
-        // 3) 실제 전송
-        mailSender.send(message);
-    }
+    void sendVerificationEmail(String toEmail,
+                               String title,
+                               String htmlContent) throws MessagingException, UnsupportedEncodingException;
 }
--- src/main/java/com/fcfs/fcfs/Email/service/EmailVerificationService.java
+++ src/main/java/com/fcfs/fcfs/Email/service/EmailVerificationService.java
@@ -1,92 +1,12 @@
 package com.fcfs.fcfs.Email.service;
 
-import com.fcfs.fcfs.Email.repository.EmailTokenRepository;
-import com.fcfs.fcfs.user.entity.User;
-import com.fcfs.fcfs.user.repository.UserRepository;
 import jakarta.mail.MessagingException;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.io.UnsupportedEncodingException;
-import java.time.Duration;
-import java.util.Optional;
-import java.util.UUID;
 
-@Slf4j(topic = "EmailVerificationService")
-@Service
-@RequiredArgsConstructor
-@Transactional
-public class EmailVerificationService {
+public interface EmailVerificationService {
 
-    private final UserRepository userRepository;
-    private final EmailService emailService;
-    private final EmailTokenRepository emailTokenRepository;
+    void sendVerificationEmail(String email) throws MessagingException, UnsupportedEncodingException;
 
-    // 이메일 링크 유효시간 5분
-    private static final Duration EMAIL_VERIFICATION_DURATION = Duration.ofMinutes(5);
-
-    // 인증링크URL
-    @Value("${app.base-url}")
-    private String baseUrl;
-
-    // 이메일 템플릿
-    @Value("${app.email.verification-template}")
-    private String verificationTemplate;
-
-    // 이메일 제목
-    @Value("${app.email.title}")
-    private String verificationTitle;
-
-    public void sendVerificationEmail(String email) throws MessagingException, UnsupportedEncodingException {
-        // 회원가입
-        Optional<User> user = Optional.ofNullable(userRepository.findByEmail(email).orElseThrow(
-                () -> new IllegalArgumentException("회원가입 절차를 제대로 진행해주세요")
-        ));
-
-        if (user.get().getIsVerified()) {
-            throw new IllegalArgumentException("이미 이메일 인증이 완료된 회원입니다.");
-        }
-
-        String token = UUID.randomUUID().toString();
-        String userId = user.get().getId().toString();
-        emailTokenRepository.saveToken(token, userId, EMAIL_VERIFICATION_DURATION);
-
-        // 이메일 전송
-        String verifyUrl = baseUrl + "/api/verify?token=" + token;
-
-        String emailText = verificationTemplate
-                .replace("[[USER_EMAIL]]", user.get().getEmail())
-                .replace("[[VERIFY_URL]]", verifyUrl);
-
-        emailService.sendVerificationEmail(email, verificationTitle, emailText);
-    }
-
-    public void verifyLinkToken(String token) {
-
-        // 1. redis에서 토큰에 매핑된 UserId 조회
-        String userId = emailTokenRepository.getUserIdByToken(token);
-        log.info("이메일 인증 회원 id찾기 : {}", userId);
-        if (userId == null) {
-            throw new IllegalArgumentException("유효하지 않거나 만료된 링크입니다.");
-        }
-
-        // 2. User 엔티티 조회
-        Optional<User> user = userRepository.findById(Long.parseLong(userId));
-
-        // 3. 이미 인증된 상태라면 토큰만 지운다
-        if (user.get().getIsVerified()) {
-            emailTokenRepository.deleteToken(token);
-            throw new IllegalArgumentException("이미 인증된 회원입니다.");
-        }
-
-        // 4. 회원의 isVerified 컬럼을 인증해주거나,
-        user.get().setIsVerified(true);
-        userRepository.save(user.get());
-
-        // 5. redis 토큰 삭제
-        emailTokenRepository.deleteToken(token);
-    }
+    void verifyLinkToken(String token);
 }
