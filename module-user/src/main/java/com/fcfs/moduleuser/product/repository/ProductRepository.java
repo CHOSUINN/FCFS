d465d90610e7ec32cab9a43cb7382343771c5bc5
SUICHO <65757661+whtndls@users.noreply.github.com>
2025-06-06 18:11:52 +0900

    remove: domain separation

--- module-user/src/main/java/com/fcfs/moduleuser/product/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fcfs.moduleuser.product.repository;
-
-import com.fcfs.moduleuser.product.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
