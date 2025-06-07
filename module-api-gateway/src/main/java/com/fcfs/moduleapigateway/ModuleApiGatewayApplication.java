69a62abd9beb4007cee3f900933e2afc4301d88b
SUICHO <65757661+whtndls@users.noreply.github.com>
2025-06-08 06:24:31 +0900

    feat: Api Gateway 도입 및 module-order api 연결

---
+++ module-api-gateway/src/main/java/com/fcfs/moduleapigateway/ModuleApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.fcfs.moduleapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleApiGatewayApplication.class, args);
+    }
+
+}
---
+++ module-api-gateway/src/test/java/com/fcfs/moduleapigateway/ModuleApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcfs.moduleapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
