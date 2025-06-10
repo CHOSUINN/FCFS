073fc0db0a386d02b4cd942968ff4792c567a532
SUICHO <65757661+whtndls@users.noreply.github.com>
2025-06-11 07:42:49 +0900

    remove: 사용하지 않는 내부 서버 요청 삭제

--- module-user/src/main/java/com/fcfs/moduleuser/client/ProductFeignClient.java
+++ module-user/src/main/java/com/fcfs/moduleuser/client/ProductFeignClient.java
@@ -5,8 +5,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.List;
-
 @FeignClient(name = "product-service",
         url = "${services.product.url}",
         fallback = ProductFeignFallback.class
@@ -15,8 +13,5 @@ public interface ProductFeignClient {
 
     @GetMapping("/api/products/{productId}")
     ProductResponseDto getProductById(@PathVariable("productId") Long productId);
-
-    @GetMapping("/api/products")
-    List<ProductResponseDto> getProducts();
 }
 
--- module-user/src/main/java/com/fcfs/moduleuser/client/ProductFeignFallback.java
+++ module-user/src/main/java/com/fcfs/moduleuser/client/ProductFeignFallback.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Slf4j(topic = "ProductFeignFallback")
 @Component
@@ -25,23 +24,5 @@ public class ProductFeignFallback implements ProductFeignClient {
                 LocalDateTime.now(),
                 null
         );
-    }
-
-    @Override
-    public List<ProductResponseDto> getProducts() {
-        log.warn("[Fallback] 상품 호출 서비스 장애!");
-        // 더미 데이터 리스트(혹은 아예 빈 리스트 반환)
-        ProductResponseDto fallbackProduct = new ProductResponseDto(
-                -1L,
-                "[서비스 장애] 상품 목록을 불러올 수 없습니다.",
-                "",
-                0,
-                0,
-                LocalDateTime.now(),
-                LocalDateTime.now(),
-                null
-        );
-        return List.of(fallbackProduct);
-        // 또는 return List.of();   ← 정말 아무것도 안 주고 싶을 때
     }
 }
