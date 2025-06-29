5347837e8d8ab2adb467d2da8aca2b5559d978b5
SUICHO <65757661+whtndls@users.noreply.github.com>
2025-06-30 00:38:43 +0900

    feat: 결제 프로세스 모듈 추가

---
+++ module-payment/src/test/java/com/microservices/demo/modulepayment/ModulePaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.demo.modulepayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModulePaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
