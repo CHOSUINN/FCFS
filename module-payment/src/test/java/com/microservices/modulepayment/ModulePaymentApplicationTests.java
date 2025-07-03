e0b4fc11ed035893d6abbbb8c0579775cb8cf206
SUICHO <65757661+whtndls@users.noreply.github.com>
2025-07-03 12:43:07 +0900

    feat: module-payment 재생성

--- module-payment/src/test/java/com/microservices/modulepayment/ModulePaymentApplicationTests.java
+++ module-payment/src/test/java/com/microservices/modulepayment/ModulePaymentApplicationTests.java
@@ -1,4 +1,4 @@
-package com.microservices.demo.modulepayment;
+package com.microservices.modulepayment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ModulePaymentApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
